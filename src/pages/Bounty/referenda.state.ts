27af1c06abc0a9bf969f05c045a130716cfdd9b1
Carlo Sala <carlosalag@protonmail.com>
2024-11-20 14:37:19 +0100

    add tippers

--- src/pages/Bounty/referenda.state.ts
+++ src/pages/Bounty/referenda.state.ts
@@ -30,6 +30,8 @@ const spenderReferenda$ = defer(
         "SmallSpender",
         "MediumSpender",
         "BigSpender",
+        "SmallTipper",
+        "BigTipper",
       ].includes(v.origin.value.type),
   ),
   toArray(),
